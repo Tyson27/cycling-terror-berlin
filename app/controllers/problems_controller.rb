d4b822769f703111ec5089b8d28e3b197745015b
alekos ittner <alekos96@freenet.de>
2020-08-26 15:28:59 +0200

    Controller

---
+++ app/controllers/problems_controller.rb
@@ -0,0 +1,51 @@
+class ProblemsController < ApplicationController
+
+  def index
+    @problems = Problem.all
+  end
+
+  def show
+    @problem = Problem.find(params[:id])
+  end
+
+  def new
+    @problem = Problem.new
+  end
+
+  def create
+    @problem = Problem.new(problem_params)
+
+    # if @problem.save
+    #   redirect_to @problem, notice: 'Problem was successfully created.'
+    # else
+    #   render :new
+    # end
+  end
+
+  def edit
+  end
+
+  def update
+    # if @problem.update(problem_params)
+    #   redirect_to @problem, notice: 'Problem was successfully updated.'
+    # else
+    #   render :edit
+    # end
+
+  def destroy
+    @problem.destroy
+    # redirect_to ..._url, notice: 'Problem was successfully destroyed'
+  end
+
+  private
+
+  def set_problem
+    @problem = Problem.find(params[:id])
+  end
+
+  def problem_params
+    params.require(:problem).permit(:category, :description, :location)
+  end
+
+
+end
