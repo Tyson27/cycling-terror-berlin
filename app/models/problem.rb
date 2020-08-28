3acb451bf37e36adbd948ed2e1feb692b20b7820
Rafael Casique <recasique@gmail.com>
2020-08-28 16:07:39 +0200

    toni checked

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,9 +1,10 @@
 class Problem < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
-  
+  acts_as_favoritable
+
   CATEGORIES = ["Glass", "Construction Work", "Cycling Path"]
-  
+
   belongs_to :user
 
   validates :category, presence: true
